58262163c05d4d5e81b1e99b26909b2e29d659ef
saurabh-gaonkar <saurabhg@axioned.com>
2022-05-18 19:29:59 +0530

    jquery added

--- main.js
+++ main.js
@@ -1,26 +1,22 @@
 // import $ from 'jquery';
-// $(document).ready(function(){
-//     console.log("dhjh");
-// })
+$(document).ready(function(){
+    console.log("dhjh");
+})
 let validzip = /^[0-9]{5,5}$/;
-let zipform = document.querySelector(".zipform");
-let instanterror = document.querySelector(".instanterror");
-let repairerror = document.querySelector(".repairerror");
-let instantform = document.querySelector(".instant-form");
-zipform.addEventListener("submit", (e) => {
+$(".zipform").submit((e) => {
     e.preventDefault();
-    repairerror.innerText = "";
-    let zip_repair_plans = document.querySelector(".zip-repair-plans").value;
+    $(".repairerror").text("");
+    let zip_repair_plans = $(".zip-repair-plans").val();
     console.log(zip_repair_plans);
     if (!(zip_repair_plans.match(validzip))) {
-        repairerror.innerText = "Please input a 5 digit ZIP Code";
+        $(".repairerror").text("Please input a 5 digit ZIP Code");
     }
 })
-instantform.addEventListener("submit", (e) => {
+    $(".instant-form").submit((e) => {
     e.preventDefault();
-    instanterror.innerText = "";
-    let zip_instant_quote = document.querySelector(".zip-instant-quote").value;
+    $(".instanterror").text("");
+    let zip_instant_quote = $(".zip-instant-quote").val();
     if (!(zip_instant_quote.match(validzip))) {
-        instanterror.innerText = "Please input a 5 digit ZIP Code";
+        $(".instanterror").text("Please input a 5 digit ZIP Code");
     }
 })
